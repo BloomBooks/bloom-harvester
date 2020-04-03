042ea14da9f72cc6fbfb1dc1d50c48e6bc8ab0ba
Jeffrey Su <jeffrey_su@sil.org>
2020-04-03 09:25:00 -0500

    Add NSubstitute mock unit tests (for ProcessOneBook() test)

---
+++ src/Harvester/WebLibraryIntegration/HarvesterBookTransfer.cs
@@ -0,0 +1,28 @@
+﻿using Bloom;
+using Bloom.WebLibraryIntegration;
+
+namespace BloomHarvester.WebLibraryIntegration
+{
+	internal interface IBookTransfer
+	{
+		string HandleDownloadWithoutProgress(string url, string destRoot);
+	}
+
+	/// <summary>
+	/// This class is nearly exactly the same as Bloom's version of BookTransfer,
+	/// except we mark that it implements the IBookTransfer interace (to make our unit testing life easier)
+	/// </summary>
+	class HarvesterBookTransfer : BookTransfer, IBookTransfer
+	{
+		internal HarvesterBookTransfer(BloomParseClient parseClient, BloomS3Client bloomS3Client, BookThumbNailer htmlThumbnailer)
+			: base(parseClient, bloomS3Client, htmlThumbnailer, new Bloom.BookDownloadStartingEvent())
+		{
+		}
+
+		public new string HandleDownloadWithoutProgress(string url, string destRoot)
+		{
+			// Just need to declare this as public instead of internal (interfaces...)
+			return base.HandleDownloadWithoutProgress(url, destRoot);
+		}
+	}
+}
