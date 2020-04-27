f4da4709dab5839b6871a966978b31eaceba3580
Jeffrey Su <jeffrey_su@sil.org>
2020-04-27 18:04:51 -0500

    Add unit tests for handling fallback families (BH-5426)

--- src/HarvesterTests/FontCheckerTests.cs
+++ src/HarvesterTests/FontCheckerTests.cs
@@ -16,7 +16,31 @@ namespace BloomHarvesterTests
 
 			List<string> missingFontsResult = FontChecker.GetMissingFonts(fontNamesUsedInBook);
 
-			Assert.AreEqual(0, missingFontsResult.Count, "No missing fonts were expected.");
+			Assert.That(missingFontsResult.Count, Is.EqualTo(0), "No missing fonts were expected.");
+		}
+
+		[Test]
+		public void GetMissingFonts_BookIsMissingFont_MarkedAsMissing()
+		{
+			string fontName = "qwertuiopasdfghjk";	// a gibberish font name
+			IEnumerable<string> fontNamesUsedInBook = new string[] { fontName };
+
+			List<string> missingFontsResult = FontChecker.GetMissingFonts(fontNamesUsedInBook);
+
+			Assert.That(missingFontsResult.Count, Is.EqualTo(1));
+			Assert.That(missingFontsResult.First(), Is.EqualTo(fontName));
+		}
+
+		[TestCase("serif")]
+		[TestCase("sans-serif")]
+		[TestCase("monospace")]
+		public void GetMissingFonts_BookContainsFontFamily_NotMarkedAsMissing(string fontFamily)
+		{
+			IEnumerable<string> fontNamesUsedInBook = new string[] { fontFamily }; // Assumes that Arial is on the machine running the test
+
+			List<string> missingFontsResult = FontChecker.GetMissingFonts(fontNamesUsedInBook);
+
+			Assert.That(missingFontsResult.Count, Is.EqualTo(0), "No error should be reported if the only missing font is font family");
 		}
 	}
 }
